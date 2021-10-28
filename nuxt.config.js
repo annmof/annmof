02c4d99de298cecfba29c8500acfe7a1d7ab8c26
majunon <fayardjul@gmail.com>
2021-10-28 14:56:41 +0200

    add logo & first slide

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,14 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    "~assets/css/global.css",
   ],
+
+  googleFonts: {
+    families: {
+      'Paytone One': true,
+    }
+  },
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
@@ -35,6 +42,7 @@ export default {
     // https://go.nuxtjs.dev/eslint
     '@nuxtjs/eslint-module',
     '@nuxtjs/tailwindcss',
+    '@nuxtjs/google-fonts'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,17 @@
 <template>
   <div>
+    <div class="h-screen">
+      <img class="m-auto" src="../assets/images/logo_annmof.png" >
+      <h1 class="text-9xl text-center">ann mof</h1>
+    </div>
+    <div class="bg-pink-100">
+      <p class="text-9xl text-center text-purple-500 p-16">Rédactrice web indépendante</p>
+      <p class="text-9xl text-center text-yellow-500 transform scale-150">& SEO</p>
+      <p class="text-6xl text-center text-green-500 pt-16">Et si on vous taillez une</p>
+      <p class="text-9xl text-center text-green-500 pt-8">stratégie digitale</p>
+      <p class="text-6xl text-center text-green-500 pt-16 underline">sur-mesure<p>
+      <p class="text-9xl text-center text-green-500 p-16">?</p>
+    </div>
     <div id="container">
       <div class="bg-pink-200 min-h-screen"></div>
       <div class="bg-purple-200 min-h-screen"></div>
@@ -25,5 +37,10 @@ export default {
 </script>
 
 <style scoped>
-
+  h1{
+    font-family: "Bimbo";
+  }
+  p{
+    font-family: "Paytone One"
+  }
 </style>
