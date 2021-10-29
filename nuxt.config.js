a11afefe2ef775557ef7f5335b141de6ff54cba4
majunon <fayardjul@gmail.com>
2021-10-29 17:57:34 +0200

    tarte au citron + GA

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,6 +17,66 @@ export default {
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ],
+    __dangerouslyDisableSanitizers: ['script'],
+    script: [
+      {
+        hid: 'tac-script1',
+        src: 'tarteaucitron.js-1.9.5/tarteaucitron.js'
+      },
+      {
+        hid: 'tac-script2',
+        innerHTML: `
+          tarteaucitron.init({
+            "privacyUrl": "", /* Privacy policy url */
+    
+            "hashtag": "#tarteaucitron", /* Open the panel with this hashtag */
+            "cookieName": "tarteaucitron", /* Cookie name */
+        
+            "orientation": "middle", /* Banner position (top - bottom) */
+          
+              "groupServices": false, /* Group services by category */
+                              
+            "showAlertSmall": false, /* Show the small banner on bottom right */
+            "cookieslist": false, /* Show the cookie list */
+                              
+              "closePopup": false, /* Show a close X on the banner */
+    
+              "showIcon": true, /* Show cookie icon to manage cookies */
+              //"iconSrc": "", /* Optionnal: URL or base64 encoded image */
+              "iconPosition": "BottomRight", /* BottomRight, BottomLeft, TopRight and TopLeft */
+    
+            "adblocker": false, /* Show a Warning if an adblocker is detected */
+                              
+              "DenyAllCta" : true, /* Show the deny all button */
+              "AcceptAllCta" : true, /* Show the accept all button when highPrivacy on */
+              "highPrivacy": true, /* HIGHLY RECOMMANDED Disable auto consent */
+                              
+            "handleBrowserDNTRequest": false, /* If Do Not Track == 1, disallow all */
+    
+            "removeCredit": false, /* Remove credit link */
+            "moreInfoLink": true, /* Show more info link */
+    
+              "useExternalCss": false, /* If false, the tarteaucitron.css file will be loaded */
+              "useExternalJs": false, /* If false, the tarteaucitron.js file will be loaded */
+    
+            //"cookieDomain": ".my-multisite-domaine.fr", /* Shared cookie for multisite */
+                              
+              "readmoreLink": "", /* Change the default readmore link */
+    
+              "mandatory": true, /* Show a message about mandatory cookies */
+          });
+        `
+      },
+      {
+        hid: 'tac-script3',
+        innerHTML: `
+          tarteaucitron.user.gtagUa = 'UA-152961193-1';
+          tarteaucitron.user.gtagMore = function () { /* add here your optionnal gtag() */ };
+          (tarteaucitron.job = tarteaucitron.job || []).push('gtag');
+        `,
+        body: true
+      }
+    ]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
@@ -32,7 +92,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '~/plugins/analytics.js'
+    
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
--- plugins/analytics.js
+++
@@ -1,8 +0,0 @@
-// analytics.js
-
-import Vue from 'vue'
-import VueGtag from 'vue-gtag'
-
-Vue.use(VueGtag, {
-  config: { id: '291735625' }
-})
